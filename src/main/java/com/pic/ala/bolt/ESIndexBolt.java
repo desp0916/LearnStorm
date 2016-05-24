2ca72554023d934443efcf206c1bc6893909080f
root <desp0916@gmail.com>
2016-05-24 15:01:08 +0800

    Updated.

--- src/main/java/com/pic/ala/bolt/ESIndexBolt.java
+++ src/main/java/com/pic/ala/bolt/ESIndexBolt.java
@@ -197,7 +197,6 @@ public class ESIndexBolt extends BaseRichBolt {
 
 		try {
 
-			System.out.println("### index: " + index.toLowerCase());
 			IndexResponse response = client
 					.prepareIndex(ES_INDEX_PREFIX + index.toLowerCase()
 						+ "-" + logDate, type.toLowerCase())
